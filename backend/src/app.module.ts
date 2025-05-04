c3486f410f368f409464db16901c9b7048fa17f7
Herman S. <dreamscache.d@gmail.com>
2025-05-04 19:38:18 +0200

    Fix root path in ServeStaticModule config (#12)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,3 +1,6 @@
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { ServeStaticModule } from "@nestjs/serve-static";
@@ -6,11 +9,14 @@ import { AppController } from "./app.controller.js";
 import { AppService } from "./app.service.js";
 import { PreferencesModule } from "./preferences/preferences.module.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true, envFilePath: [".env", ".env.local"] }),
 		ServeStaticModule.forRoot({
-			rootPath: "static",
+			rootPath: join(__dirname, "..", "static"),
 			serveStaticOptions: {
 				dotfiles: "ignore",
 				maxAge: 3600e3 // 1 hour TTL
