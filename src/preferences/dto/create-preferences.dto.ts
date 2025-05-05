7541383f271ded34640ddf9984379cd9acba1149
Herman S. <dreamscache.d@gmail.com>
2025-05-05 21:22:37 +0200

    Update README (#19)

--- src/preferences/dto/create-preferences.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsArray, IsString } from "class-validator";
-
-export class CreatePreferencesDto {
-	@IsArray()
-	@IsString({ each: true })
-	favoriteTickers: string[];
-}
--- src/preferences/dto/update-preferences.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from "@nestjs/mapped-types";
-
-import { CreatePreferencesDto } from "./create-preferences.dto.js";
-
-export class UpdatePreferencesDto extends PartialType(CreatePreferencesDto) {}
