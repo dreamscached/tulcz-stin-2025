5865cdfe73502aa861037dd3fba2ea420e5313b2
Herman S. <dreamscache.d@gmail.com>
2025-05-10 14:49:42 +0200

    Define constant path for IEX data

--- src/preferences/preferences.constants.ts
+++ src/preferences/preferences.constants.ts
@@ -1,1 +1,5 @@
-export const PREFERENCES_PATH = "./files/preferences.json";
+import { join } from "node:path";
+
+import { FILES_DIR } from "src/app.constants.js";
+
+export const PREFERENCES_PATH = join(FILES_DIR, "preferences.json");
--- src/tiingo/tiingo.constants.ts
+++ src/tiingo/tiingo.constants.ts
@@ -1,1 +1,6 @@
+import { join } from "node:path";
+
+import { FILES_DIR } from "src/app.constants.js";
+
 export const BASE_URL = "https://api.tiingo.com";
+export const IEX_DB_PATH = join(FILES_DIR, "iex_db.json");
